cded4a34b4c7e53a8c9e8fe47ba491056d8b12c4
vivanch <vivanch@softserveinc.com>
2019-09-12 15:26:47 +0300

    added privacy policy

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict'
+const {join} = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
@@ -7,11 +8,26 @@ const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(join(__dirname, '/public'), {
+    index: false   // to ignore index.html as a static file. We need it for server rendering;
+}));
+
+// Setting up template engines;
+app.engine('html', require('ejs').renderFile);
+
+// Configuring Express.js App Settings;
+app.set('view engine', 'html');
+app.set('views', join(__dirname, '/public'));
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.send('Hi I am a chatbot...');
+});
+
+app.get('/privacy-policy', function(req, res) {
+    res.render('index.html', { req });
 });
 
 app.get('/webhook/', function(req, res) {
