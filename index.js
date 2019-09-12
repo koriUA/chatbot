fc0800a1c961831fe69bb4f9e1bf612933808c86
vivanch <vivanch@softserveinc.com>
2019-09-12 15:41:10 +0300

    facebook

--- index.js
+++ index.js
@@ -37,6 +37,40 @@ app.get('/webhook/', function(req, res) {
     res.send('Wrong token...');
 });
 
+app.post('/webhook/', function(req, res) {
+   let messaging_events = req.body.entry[0].messaging_events;
+   for (let i = 0; i < messaging_events.length; i++) {
+       let event = messaging_events[i];
+       let sender = event.sender.id;
+       if (event.message && event.message.text) {
+           let text = event.message.text;
+           sendText(sender, "Text echo: " + text.subscting(0, 100));
+       }
+   }
+});
+
+const token = "EAAJo2B7RWcYBAAiTKNgeCZBQyDNgUzo1AZB4P1jfbUkvs0K8xj48bJoOVM7OfHaFRv0SogRsqh5yIWUDjVOxMsDBOf7RDV2XQTxbEyqZAnr8bxuyJAGgVFggogFD6ByoePtKYjtb1m0Qmae4ROby5E4bPuPvnqmx7nEdHcGoAZDZD";
+const access_token = token;
+
+function sendText(sender, text) {
+    let messageData = {text: text};
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: {access_token, token},
+        method: "POST",
+        json: {
+            receipt: {id: sender},
+            message: messageData
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log("sending error");
+        } else if (response.body.error) {
+            console.log("response body error");
+        }
+    });
+}
+
 app.listen(app.get('port'), function() {
     console.log('running: port', app.get('port'));
 });
