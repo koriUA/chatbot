e7853251c96f89bb79ab520a765648bfc3d85191
vivanch <vivanch@softserveinc.com>
2019-09-14 20:17:24 +0300

    added messages

--- index.js
+++ index.js
@@ -27,10 +27,12 @@ app.get('/', function(req, res) {
 });
 
 app.get('/privacy-policy', function(req, res) {
+    console.log('/privacy-policy');
     res.render('index.html', { req });
 });
 
 app.get('/webhook/', function(req, res) {
+    console.log('GET /webhook/');
     if (req.query['hub.verify_token'] === 'blondiebytes') {
         res.send(req.query['hub.challenge']);
     }
@@ -38,6 +40,7 @@ app.get('/webhook/', function(req, res) {
 });
 
 app.post('/webhook/', function(req, res) {
+    console.log('POST /webhook/');
    let messaging_events = req.body.entry[0].messaging;
    for (let i = 0; i < messaging_events.length; i++) {
        let event = messaging_events[i];
