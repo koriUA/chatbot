0d4a5d188fe1ce3f5aa535d46eba636d286e241d
vivanch <vivanch@softserveinc.com>
2019-09-17 14:58:03 +0300

    chatbot

--- index.js
+++ index.js
@@ -22,31 +22,35 @@ app.set('views', join(__dirname, '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send('Hi I am a chatbot...');
 });
 
-app.get('/privacy-policy', function(req, res) {
-    res.render('index.html', { req });
+app.get('/privacy-policy', function (req, res) {
+    res.render('index.html', {req});
 });
 
-app.get('/webhook/', function(req, res) {
+app.get('/test', function (req, res) {
+    res.render('test.html', {req});
+});
+
+app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'blondiebytes') {
         res.status(200).send(req.query['hub.challenge']);
     }
     res.send('Wrong token...');
 });
 
-app.post('/webhook/', function(req, res) {
-   let messaging_events = req.body.entry[0].messaging;
-   for (let i = 0; i < messaging_events.length; i++) {
-       let event = messaging_events[i];
-       let sender = event.sender.id;
-       if (event.message && event.message.text) {
-           let text = event.message.text;
-           sendText(sender, "Text echo: " + text.substring(0, 100));
-       }
-   }
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging;
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = messaging_events[i];
+        let sender = event.sender.id;
+        if (event.message && event.message.text) {
+            let text = event.message.text;
+            sendText(sender, "Text echo: " + text.substring(0, 100));
+        }
+    }
     // Returns a '200 OK' response to all requests
     res.status(200).send('EVENT_RECEIVED');
 });
@@ -55,16 +59,18 @@ const access_token = "EAAJo2B7RWcYBAACxZBdxb5nUKtV04ulydX5WUwPpJ8aYHW7zK3Hs09nmc
 
 function sendText(sender, text) {
     let messageData = {text: text};
+    if (text.includes('options')) {
+        return getMessageTemplate(sender, text);
+    }
     request({
         url: "https://graph.facebook.com/v4.0/me/messages",
         qs: {access_token},
         method: "POST",
         json: {
             recipient: {id: sender},
-            message: messageData,
-            //subscribed_fields: 'messages'
+            message: messageData
         }
-    }, function(error, response, body) {
+    }, function (error, response, body) {
         if (error) {
             console.log("error");
         } else if (response.body.error) {
@@ -73,10 +79,63 @@ function sendText(sender, text) {
     });
 }
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('running: port', app.get('port'));
 });
 
+function getTemplate() {
+    return {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [
+                    {
+                        "title": "Welcome!",
+                        "image_url": "https://petersfancybrownhats.com/company_image.png",
+                        "subtitle": "We have the right hat for everyone.",
+                        "default_action": {
+                            "type": "web_url",
+                            "url": "https://petersfancybrownhats.com/view?item=103",
+                            "webview_height_ratio": "tall",
+                        },
+                        "buttons": [
+                            {
+                                "type": "web_url",
+                                "url": "https://petersfancybrownhats.com",
+                                "title": "View Website"
+                            }, {
+                                "type": "postback",
+                                "title": "Start Chatting",
+                                "payload": "DEVELOPER_DEFINED_PAYLOAD"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    };
+}
 
-//heroku logs --tail --app chatbot432
+function getMessageTemplate(sender, text) {
+    request({
+        url: "https://graph.facebook.com/v4.0/me/messages",
+        qs: {access_token},
+        method: "POST",
+        json: {
+            recipient: {id: sender},
+            message: getTemplate()
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log("error");
+        } else if (response.body.error) {
+            console.log("response body error");
+        }
+    });
+}
+
+
+// https://www.messenger.com/t/109134463806552
+// heroku logs --tail --app chatbot432
 
